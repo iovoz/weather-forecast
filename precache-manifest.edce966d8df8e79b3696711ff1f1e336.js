226c3a2d40b07d3c106231007ba7cc5f95c8060e
Roy <symbiane@sina.com>
2019-11-08 19:11:58 +0800

    Updates

---
+++ precache-manifest.edce966d8df8e79b3696711ff1f1e336.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffeddcafd47799c5ffc01f61232d41e",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "de3b4c17ce9ac19118f7",
+    "url": "/weather-forecast/static/css/2.9dc5507a.chunk.css"
+  },
+  {
+    "revision": "3bfd7915008c02e6acbb",
+    "url": "/weather-forecast/static/css/main.2eb651d1.chunk.css"
+  },
+  {
+    "revision": "de3b4c17ce9ac19118f7",
+    "url": "/weather-forecast/static/js/2.9d81c3d0.chunk.js"
+  },
+  {
+    "revision": "3bfd7915008c02e6acbb",
+    "url": "/weather-forecast/static/js/main.2be6753c.chunk.js"
+  },
+  {
+    "revision": "21b2cd4bae08677303ef",
+    "url": "/weather-forecast/static/js/runtime-main.69c63950.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/weather-forecast/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/weather-forecast/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/weather-forecast/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/weather-forecast/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/weather-forecast/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
